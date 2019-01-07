16c77e7cee920c18dd56ede52704184c4e3ab587
nobody246 <a.se8seven@gmail.com>
2019-01-06 19:09:07 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+/etc/init.d/tor restart
+sleep 2
 while true;
 do
-    #/etc/init.d/tor restart
-    sleep 2
     for var in `seq 1 $2`; 
     do 
         ./fu.sh $1 &
     done;
     sleep $3
+    /etc/init.d/tor restart
+    sleep 2
 done
